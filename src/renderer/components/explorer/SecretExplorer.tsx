4cfd3100efcf04f3e19fb9ff519234ff94faaa0f
Jonas Verhoelen <jonas.verhoelen@codecentric.de>
2018-10-30 12:53:50 +0100

    small refactoring; no sample data anymore

--- src/renderer/components/explorer/SecretExplorer.tsx
+++ src/renderer/components/explorer/SecretExplorer.tsx
@@ -1,19 +1,23 @@
 import TreeComponent, { Tree } from './TreeComponent'
 import * as React from 'react'
 import Gopass from '../../service/Gopass'
-import ExampleTreeData from './ExampleTreeData'
 import SecretsDirectoryService from '../../service/SecretsDirectoryService'
 
-interface SecretExplorerState {
-    tree?: Tree
-}
-
 const testLeafClick = async (secretName: string) => console.log(await Gopass.show(secretName))
+interface SecretExplorerState {
+    tree: Tree
+}
 
 export default class SecretExplorer extends React.Component<any, SecretExplorerState> {
     constructor(props: any) {
         super(props)
-        this.state = { tree: undefined }
+        this.state = {
+            tree: {
+                name: 'Stores',
+                toggled: true,
+                children: []
+            }
+        }
     }
 
     async componentDidMount() {
@@ -24,11 +28,6 @@ export default class SecretExplorer extends React.Component<any, SecretExplorerS
     }
 
     render() {
-        return (
-            <TreeComponent
-                tree={this.state.tree ? this.state.tree : ExampleTreeData}
-                onLeafClick={testLeafClick}
-            />
-        )
+        return <TreeComponent tree={this.state.tree} onLeafClick={testLeafClick} />
     }
 }
