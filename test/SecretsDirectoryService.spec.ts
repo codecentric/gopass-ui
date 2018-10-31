a756f5b87b1c4b1c131530188dbcb3110609b3a7
Jonas Verhoelen <jonas.verhoelen@codecentric.de>
2018-10-31 12:45:01 +0100

    fix

--- test/SecretsDirectoryService.spec.ts
+++ test/SecretsDirectoryService.spec.ts
@@ -1,5 +1,5 @@
 import SecretsDirectoryService from '../src/renderer/secrets/SecretsDirectoryService'
-import { Tree } from '../src/renderer/components/explorer/TreeComponent'
+import { Tree } from '../src/renderer/explorer/TreeComponent'
 
 describe('SecretsDirectoryService', () => {
     it('should transform a list of secret names into tree structure', () => {
