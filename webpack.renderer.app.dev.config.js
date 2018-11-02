d124b208080fad219988ab1d9a24b49efb1e70f2
Jonas Verhoelen <jonas.verhoelen@codecentric.de>
2018-11-02 17:41:19 +0100

    fix renderer build

--- webpack.renderer.app.dev.config.js
+++ webpack.renderer.app.dev.config.js
@@ -14,7 +14,7 @@ module.exports = merge.smart(baseConfig, {
         rules: [
             {
                 test: /\.tsx?$/,
-                include: [ path.resolve(__dirname, 'src', 'renderer'), path.resolve(__dirname, 'src', 'shared') ],
+                include: [ path.resolve(__dirname, 'src', 'renderer') ],
                 loaders: [ 'react-hot-loader/webpack', 'awesome-typescript-loader' ]
             }
         ]
