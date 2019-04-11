07ecbcb6651f40d4cc4cab1c0e2f0fdd1b041fb1
Jonas Verhoelen <jonas.verhoelen@codecentric.de>
2019-04-11 23:07:49 +0200

    linting

--- src/renderer/explorer-app/content/components/EnvironmentTest.tsx
+++ src/renderer/explorer-app/content/components/EnvironmentTest.tsx
@@ -45,6 +45,7 @@ export function EnvironmentTest() {
             return <RunningContent/>
         case 'OK':
             return <OkContent/>
+        default:
         case 'ERROR':
             return <ErrorContent errorDetails={errorDetails} reset={reset}/>
     }
