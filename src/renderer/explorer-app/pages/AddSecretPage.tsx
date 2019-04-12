80cabf4f98a7b0c68ac797322bb12d4ed4dc944f
Jonas Verhoelen <jonas.verhoelen@codecentric.de>
2019-04-12 22:05:32 +0200

    password health indicator for add-secrets

--- src/renderer/explorer-app/pages/AddSecretPage.tsx
+++ src/renderer/explorer-app/pages/AddSecretPage.tsx
@@ -3,6 +3,8 @@ import * as m from 'react-materialize'
 import { withRouter, RouteComponentProps } from 'react-router'
 import Gopass from '../../secrets/Gopass'
 import { passwordSecretRegex } from '../../secrets/deriveIconFromSecretName'
+import { PasswordStrengthInfo } from '../components/PasswordStrengthInfo'
+import { PasswordRater } from '../../password-health/PasswordRater'
 
 interface AddSecretPageState {
     name?: string
@@ -21,13 +23,15 @@ class AddSecretPage extends React.Component<RouteComponentProps, AddSecretPageSt
     public render() {
         const { name, value } = this.state
         const nameIndicatesPassword = name ? passwordSecretRegex.test(name) : false
+        const entity = nameIndicatesPassword ? 'Password' : 'Secret'
         const nameLabel = `Secret name (${nameIndicatesPassword ? 'detected password' : 'e.g. store/my/new/secret/name'})`
-        const valueLabel = `${nameIndicatesPassword ? 'Password' : 'Secret'} value`
+        const valueLabel = `${entity} value`
         const shuffleButtonLabel = `Shuffle ${nameIndicatesPassword ? 'password' : 'value'}`
+        const currentPasswordValueRating = PasswordRater.ratePassword(value || '')
 
         return (
             <>
-                <h4>New { nameIndicatesPassword ? 'Password' : 'Secret' }</h4>
+                <h4>New { entity }</h4>
 
                 <m.CardPanel>
                     Adds new secrets to your Gopass stores. After clicking the Add-button, your new secret will be pushed to remote directly.
@@ -36,6 +40,10 @@ class AddSecretPage extends React.Component<RouteComponentProps, AddSecretPageSt
                 <m.Row>
                     <m.Input s={12} value={name} onChange={this.changeName} label={ nameLabel } />
                     <m.Input s={12} value={value} onChange={this.changeValue} label={ valueLabel } />
+                    <PasswordStrengthInfo
+                        strength={ currentPasswordValueRating.health }
+                        labelContent={ `${entity} value strength` }
+                    />
                     <m.Col s={12}>
                         <m.Button style={{ marginRight: '10px' }} onClick={this.shuffleRandomValue} waves='light'>{ shuffleButtonLabel }</m.Button>
                         <m.Button disabled={ !name || !value } onClick={this.addSecret} waves='light'>Save</m.Button>
@@ -48,9 +56,16 @@ class AddSecretPage extends React.Component<RouteComponentProps, AddSecretPageSt
     private changeName = (_: any, name: string) => this.setState({ name })
     private changeValue = (_: any, value: string) => this.setState({ value })
 
-    private generateRandomValue = () => {
-        const randomSegment = () => Math.random().toString(36).slice(-8)
-        return `${randomSegment()}-${randomSegment()}-${randomSegment()}-${randomSegment()}`
+    private generateRandomValue = (length = 24) => {
+        const chars = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890'
+        let randomPassword = ''
+
+        for (let x = 0; x < length; x++) {
+            const randomIndex = Math.floor(Math.random() * chars.length)
+            randomPassword += chars.charAt(randomIndex)
+        }
+
+        return randomPassword
     }
 
     private shuffleRandomValue = () => {
