9fd6dcff6463d910a7d9c3e208b0276a9246118b
Matthias Rütten <matthias.ruetten@codecentric.de>
2019-04-13 14:52:06 +0200

    remove REDUX_DEVTOOLS

--- src/main/index.ts
+++ src/main/index.ts
@@ -48,7 +48,7 @@ const contextMenu = Menu.buildFromTemplate([
 const installExtensions = async () => {
     const installer = require('electron-devtools-installer')
     const forceDownload = !!process.env.UPGRADE_EXTENSIONS
-    const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS']
+    const extensions = ['REACT_DEVELOPER_TOOLS']
 
     return Promise.all(extensions.map(name => installer.default(installer[name], forceDownload))).catch(console.info)
 }
