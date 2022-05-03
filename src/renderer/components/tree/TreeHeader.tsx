34b3a7b8b05f3036e6c6e1bef8e3b0d378fc674b
Ian Mutawa <mma47@njit.edu>
2022-05-03 15:54:43 -0400

    Show both folder and chevron icons to Improve UX
    Toggling folder to chevron makes difficult to know which tree items is open and which is not. Rotating the chevron down makes to indicate an open node is much better.

--- src/renderer/components/tree/TreeHeader.tsx
+++ src/renderer/components/tree/TreeHeader.tsx
@@ -4,14 +4,19 @@ import * as m from 'react-materialize'
 import { deriveIconFromSecretName } from '../../secrets/deriveIconFromSecretName'
 
 export const TreeHeader = ({ style, node }: any) => {
-    let iconType = node.toggled ? 'chevron_right' : 'folder'
+    let iconType = 'folder'
 
+    const isLeaf = !node.children && node.path
     if (!node.children && node.path) {
         iconType = deriveIconFromSecretName(node.name)
     }
-
     return (
         <div style={style.base}>
+            {node.children && (
+                <div className={`chevron ${node.toggled && 'toggled'}`}>
+                    <m.Icon small>chevron_right</m.Icon>
+                </div>
+            )}
             <m.Icon small>{iconType}</m.Icon>
 
             {node.name}
