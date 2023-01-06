50f7851ff307aa9790bf64dbd2ea0a860b2285cc
Matthias Rütten <matthias.ruetten@gmail.com>
2023-01-06 23:21:01 +0100

    sync stores

--- src/ListHeader.tsx
+++ src/ListHeader.tsx
@@ -1,6 +1,6 @@
-import { PlusCircleTwoTone } from '@ant-design/icons'
 import * as React from 'react'
-import { Button } from 'antd'
+import { Button, Space } from 'antd'
+import { Gopass } from './Gopass'
 
 export interface ListHeaderProps {
     numberOfEntries: number
@@ -8,12 +8,24 @@ export interface ListHeaderProps {
 }
 
 export const ListHeader = ({ numberOfEntries, openAddEntryModal }: ListHeaderProps) => {
+    const [syncRunning, setSyncRunning] = React.useState(false)
+    const syncStores = async () => {
+        setSyncRunning(true)
+        await Gopass.sync()
+        setSyncRunning(false)
+    }
+
     return (
         <div className='list-header'>
             <strong>{numberOfEntries} Entries</strong>
-            <Button size='small' type='primary' onClick={openAddEntryModal}>
-                Add New Entry
-            </Button>
+            <Space direction='horizontal'>
+                <Button size='small' type='primary' onClick={openAddEntryModal}>
+                    Add New Entry
+                </Button>
+                <Button size='small' type='primary' onClick={syncStores} loading={syncRunning}>
+                    Sync
+                </Button>
+            </Space>
         </div>
     )
 }
